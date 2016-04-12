362cb5fa51a2e011b9ccfe13a0179b5839a0c959
Matt Mason <mamaso@microsoft.com>
2016-04-12 09:22:34 -0700

    Added azure push adapter

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const FilesAdapter = require('parse-server-azure-storage').AzureStorageAdapter;
+const PushAdapter = require('parse-server-azure-push');
 
 module.exports = {
-    FilesAdapter
+    FilesAdapter,
+    PushAdapter
 }
